40177d2849c526cb4d3a006ce74a5fbf705759d2
Margaritka <chip30_83@mail.ru>
2022-11-15 11:58:15 +0300

    готовый проект

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const advantageList = document.querySelectorAll('.advantage-list')
+const modal = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close-modal')
+for (const advantage of advantageList) {
+    advantage.addEventListener('click',()=>{
+        modal.classList.add('modal_opened')
+    })
+}
+closeBtn.addEventListener('click',()=>{
+    modal.classList.remove('modal_opened')
+})
+
